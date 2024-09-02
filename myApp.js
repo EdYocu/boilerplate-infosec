614bb3ea0a39d2e9beca7419d1a92f073db37c35
Edgardo Yocupicio Ruiz <edgardoyocupicio@gmail.com>
2024-09-02 10:37:34 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,55 +1,9 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet.hidePoweredBy());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
